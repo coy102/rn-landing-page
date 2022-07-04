d17c49130868447670c0c7d6a02b0c478a43af75
fajar septiawan <fajar.septiawan@zebrax.id>
2022-07-04 18:10:48 +0700

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import Home from './screens/home'
+
+const App = () => {
+  return <Home />
+}
+
+export default App
---
+++ src/screens/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ScrollView, Text } from 'react-native'
+
+function Home() {
+  return (
+    <ScrollView>
+      <Text>React Native</Text>
+    </ScrollView>
+  )
+}
+
+export default Home
