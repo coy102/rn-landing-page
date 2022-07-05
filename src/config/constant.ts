bcaa66ded8307fd57a873e272bca389e9fa253f0
fajar septiawan <fajar.septiawan@zebrax.id>
2022-07-05 16:07:19 +0700

    Change url

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,4 +1,4 @@
-export const imgUrl = 'https://my-asset.s3-website-ap-southeast-1.amazonaws.com'
+export const imgUrl = 'https://d1f4e2mng10rea.cloudfront.net/test'
 
 export const textLabel = {
   learnMore: 'Learn More',
--- src/modules/MetricList/data.ts
+++ src/modules/MetricList/data.ts
@@ -3,23 +3,23 @@ import { numberSeparator } from '../../utils/number'
 
 export const metrics = [
   {
-    img: `${imgUrl}/metric/ovo.PNG`,
+    img: `${imgUrl}/metric/ovo.png`,
     title: 'Balance',
     value: `Rp. ${numberSeparator(24000)}`,
   },
   {
-    img: `${imgUrl}/metric/pedulilindungi.PNG`,
+    img: `${imgUrl}/metric/pedulilindungi.png`,
     title: 'Scan',
     value: 'PeduliLindungi',
   },
   {
     id: 3,
-    img: `${imgUrl}/metric/wallet.PNG`,
+    img: `${imgUrl}/metric/wallet.png`,
     title: 'Top Up',
     value: 'Wallet',
   },
   {
-    img: `${imgUrl}/metric/point.PNG`,
+    img: `${imgUrl}/metric/point.png`,
     title: 'Use Points',
     value: numberSeparator(100000),
   },
